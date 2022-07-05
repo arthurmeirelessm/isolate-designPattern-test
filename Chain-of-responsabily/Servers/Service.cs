3e5d64f52a3cafc73baadc1151388444c0aaccd3
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-05 20:59:19 -0300

    FInishing service configuration

--- Chain-of-responsabily/Servers/Service.cs
+++ Chain-of-responsabily/Servers/Service.cs
@@ -23,23 +23,35 @@ namespace Chain_of_responsabily.Servers
 
         public Boolean Login(string email, string password)
         {
-            return true;
+            if(middleware.Check(email, password))
+            {
+                Console.WriteLine("Usario logado com sucesso");
+                Console.WriteLine("Seja bem vindo!");
+                return true;
+            }
+
+            return false;
         }
 
         public void RegisterUser(string email, string password)
         {
+            users[email] = password;
         }
 
 
         public Boolean HasEmail(string email)
         {
-            return true;
+            return users.ContainsKey(email);
         }
 
 
-        public Boolean IsValidPassword(string password)
+        public Boolean IsValidPassword(string email, string password)
         {
-            return true;
+            string value = "";
+
+            users.TryGetValue(email, out value);
+
+            return password == value;
         }
 
     }
