e60e58eb5315f09dcd58a6d53a9294a79f892e4e
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-06 20:34:16 -0300

    Starting chain-of-responsabily refactoring

--- Chain-of-responsabily/Program.cs
+++ Chain-of-responsabily/Program.cs
@@ -10,39 +10,8 @@ namespace Chain_of_responsabily
 {
     class Program
     {
-        private static Service service;
-
-        static void init()
-        {
-            service = new Service();
-
-            service.RegisterUser("arthur@gmail.com", "3333");
-
-            Middleware middleware = new CheckUserMiddeware(service);
-
-            middleware.LinkWith(new CheckPermissionMiddleware());
-
-            service.SetMiddleware(middleware);
-        }
-
         static void Main(string[] args)
         {
-            init();
-
-            Boolean done = false;
-
-
-            do
-            {
-                Console.WriteLine("Digite o seu email: ");
-                string email = Console.ReadLine();
-                Console.WriteLine("Digite o seu passwoprd");
-                string password = Console.ReadLine();
-
-
-                done = service.Login(email, password);
-
-            } while (!done);
         }
     }
 }
