b2787f0a67c45bb328d44aa294d3b52dd75b2f7b
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-06 02:23:03 -0300

    Finishing and testing app validation

--- Chain-of-responsabily/Middlewares/CheckPermissionMiddleware.cs
+++ Chain-of-responsabily/Middlewares/CheckPermissionMiddleware.cs
@@ -10,7 +10,7 @@ namespace Chain_of_responsabily.Middlewares
     {
         public override bool Check(string email, string password)
         {
-            if (email.Equals("arthur@gmail.com") && (password.Equals("3333")))
+            if (email.Equals("arthur@gmail.com"))
             {
                 Console.WriteLine("Login permitido!");
                 return true;
