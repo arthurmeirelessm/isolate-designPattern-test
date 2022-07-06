3b36a1be90eb7648e9cb57a56d4c9a750aca784f
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-06 20:50:59 -0300

    Add middleware file

---
+++ Chain-of-responsabily/Middlewares/IMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_of_responsabily.Middlewares
+{
+    abstract class IMiddleware
+    {
+        private IMiddleware next;
+
+        protected IMiddleware LinkWwith(IMiddleware next)
+        {
+            this.next = next;
+
+            return this;
+        }
+    }
+}
